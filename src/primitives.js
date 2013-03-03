cab9ac39741cdbfb7c3e3060423a49393ce807fd
Gregoire Divaret <gdivaret@gmail.com>
2013-03-03 03:59:21 +0100

    added cylinder

--- src/primitives.js
+++ src/primitives.js
@@ -3,29 +3,24 @@ glgl.primitives = (function(){
 	function Plane(nx, ny)
 	{
 		this.nVertices = nx * ny;
-		this.vertices = new Float32Array(3 * this.nVertices);
-		for(var i = 0; i < this.nVertices; i++)
-		{
-			var vId = 3 * i;
-			this.vertices[vId] = (i % nx) / (nx - 1);
+        this.vertices = new Float32Array(3 * this.nVertices);
+        this.uvs = new Float32Array(2 * this.nVertices);
 
-			this.vertices[vId + 1] = Math.floor(i / nx) / (ny - 1);
-			this.vertices[vId + 2] = 0;
+        var i;
+		for(i = 0; i < this.nVertices; i++)
+		{
+            var vId = 3 * i;
+            var uvID = 2 * i;
+            this.uvs[uvID] = this.vertices[vId] = (i % nx) / (nx - 1);
+            this.uvs[uvID + 1] = this.vertices[vId + 1] = Math.floor(i / nx) / (ny - 1);
+            this.vertices[vId + 2] = 0;
 		}
 
 		var nSquares = (nx - 1) * (ny - 1);
 		this.indices = new Uint16Array(6 * nSquares);
-		for(var i = 0 ; i < nSquares; i++)
+		for(i = 0 ; i < nSquares; i++)
 		{
 			var id = i * 6;
-			/*var refId = i % (nx - 1) + Math.floor(i / (nx - 1)) * nx;
-			this.indices[id] = refId;
-			this.indices[id + 1] = refId + 1;
-			this.indices[id + 2] = refId + nx + 1;
-
-			this.indices[id + 3] = refId;
-			this.indices[id + 4] = refId + nx + 1;
-			this.indices[id + 5] = refId + nx;*/
 
 			this.indices[id] = i % (nx - 1) + Math.floor(i / (nx - 1)) * nx;
 			this.indices[id + 1] = this.indices[id] + 1;
@@ -37,7 +32,7 @@ glgl.primitives = (function(){
 		}
 	}
 
-	function TetraHedron()
+	function Tetrahedron()
 	{
 
 	}
@@ -50,11 +45,56 @@ glgl.primitives = (function(){
 										  -1, -1,  1, 	 1, -1,  1, 	-1,  1,  1, 	 1,  1,  1,
 										  -1,  1, -1, 	-1, -1, -1, 	-1,  1,  1,		-1, -1,  1,
 										   1, -1, -1, 	 1,  1, -1, 	 1, -1,  1, 	 1,  1,  1,
-										  -1,  1, -1, 	 1,  1, -1, 	-1,  1,  1, 	 1,  1,  1])
+										  -1,  1, -1, 	 1,  1, -1, 	-1,  1,  1, 	 1,  1,  1]);
 	}
 
 
+    function Cylinder(nx, ny)
+    {
+        this.nVertices = nx * ny;
+        this.vertices = new Float32Array(3 * this.nVertices);
+        this.uvs = new Float32Array(2 * this.nVertices);
+
+        var i, id, col, row;
+        for(i = 0; i < this.nVertices; i++)
+        {
+            id = 3 * i;
+            col = i % nx;
+            row = Math.floor(i / nx);
+            var angle = 2 * Math.PI * (col / (nx - 1));
+            this.vertices[id] = Math.cos(angle);
+            this.vertices[id + 1] = row / (ny - 1);
+            this.vertices[id + 2] = Math.sin(angle);
+
+            var uvID = 2 * i;
+            this.uvs[uvID] = col / (nx - 1);
+            this.uvs[uvID + 1] = row / (ny - 1);
+        }
+
+        var n = this.nVertices - nx;
+        this.indices = new Uint16Array(6 * n);
+        for(i = 0; i < n; i++)
+        {
+            id = 6 * i;
+            col = i % nx;
+            row = Math.floor(i / nx);
+            if(col == nx - 1) continue;
+            var nextVertexID = row * nx + ((col + 1) % nx);
+
+            this.indices[id] = i;
+            this.indices[id + 1] = nextVertexID;
+            this.indices[id + 2] = nextVertexID + nx;
+
+            this.indices[id + 3] = i;
+            this.indices[id + 4] = nextVertexID + nx;
+            this.indices[id + 5] = i + nx;
+        }
+    }
+
+
+
 	return {Plane:Plane,
-			TetraHedron:TetraHedron,
-			Cube:Cube}
+			Tetrahedron:Tetrahedron,
+			Cube:Cube,
+            Cylinder:Cylinder}
 })();
